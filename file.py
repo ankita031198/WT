0b6e63805418eebccf7c9b690d163a9f4b9bd563
ankita031198 <48233480+ankita031198@users.noreply.github.com>
2020-01-03 12:27:39 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,37 @@
+from pandas import read_csv
+
+path = "iris.csv"
+data = read_csv(path)
+
+print(data.shape)
+print()
+print(data[:3])
+print()
+print(data.describe())
+print()
+
+count_class = data.groupby('sepal_length').size()
+
+print(count_class)
+print()
+
+correlations = data.corr(method='pearson')
+
+print(correlations)
+print()
+
+print(data.skew())
+print()
+
+from sklearn import preprocessing
+
+array = data.iloc[:, 0:4].values
+
+print(array)
+print()
+
+data_scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
+data_rescaled = data_scaler.fit_transform(array)
+
+print(data_rescaled)
+print()
